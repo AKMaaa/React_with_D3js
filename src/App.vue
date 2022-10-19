12cf5df0a843363c0de3f2cf97c88f5f9c7fb67f
Akira_Maeda <akiramaeda0905@gmail.com>
2022-10-19 17:34:16 +0900

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WordCloud />
 </template>
 
 <script>
+// ここでコンポーネントをインポートする
 import HelloWorld from './components/HelloWorld.vue'
+import WordCloud from './components/WordCloud.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    WordCloud,
   }
 }
 </script>
---
+++ src/components/WordCloud.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>This is Demo Cloud</h1>
+    </div>
+    <div id="cloud"></div>
+</template>
+
+<script>
+
+export default {
+    name: 'WordCloud'
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
